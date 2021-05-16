2c68353cfe7bbe6b86702db9e03df41e20d2f0ae
Gerges Wageh <gergeswageh2580@gmail.com>
2021-05-16 18:14:50 +0200

    Files added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
+import Products from "./components/pages/Products";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HeroSection.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import "../App.css";
+
+export default function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src="/videos/video-2.mp4" autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p> What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH TRAILER <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
+}
